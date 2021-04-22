eb90beb458ed39c527e6d78a6349a25f7c6fe7bd
Tom Smith <74121145+TomSmith98-C@users.noreply.github.com>
2021-04-22 10:31:54 -0500

    Add files via upload

---
+++ Constantes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Constantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.1416;
+
+            //Por lo regular las constantes se ponen en mayusculas para identificarlas mejor.
+            //El volumen de un cilindro se da con la formula Volumen = PI * radio^2 * h;
+
+            int h = 23;//Altura del cilindro
+            double r = 32.33222;
+
+            Console.WriteLine($"El volumen del cilindro es igual a: {PI * r * r * h}");
+        }
+    }
+}
